bbcb311881310bd03be02e52fe323c2b3076dd43
Yaaappee <yaaappee@gmail.com>
2016-02-21 00:04:31 +0200

    Add first api (api/People)

--- Data.gov.ua.Tests/Controllers/ValuesControllerTest.cs
+++ Data.gov.ua.Tests/Controllers/ValuesControllerTest.cs
@@ -17,7 +17,7 @@ namespace Data.gov.ua.Tests.Controllers
         public void Get()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            PeopleController controller = new PeopleController();
 
             // Act
             IEnumerable<string> result = controller.Get();
@@ -33,7 +33,7 @@ namespace Data.gov.ua.Tests.Controllers
         public void GetById()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            PeopleController controller = new PeopleController();
 
             // Act
             string result = controller.Get(5);
@@ -46,7 +46,7 @@ namespace Data.gov.ua.Tests.Controllers
         public void Post()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            PeopleController controller = new PeopleController();
 
             // Act
             controller.Post("value");
@@ -58,7 +58,7 @@ namespace Data.gov.ua.Tests.Controllers
         public void Put()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            PeopleController controller = new PeopleController();
 
             // Act
             controller.Put(5, "value");
@@ -70,7 +70,7 @@ namespace Data.gov.ua.Tests.Controllers
         public void Delete()
         {
             // Arrange
-            ValuesController controller = new ValuesController();
+            PeopleController controller = new PeopleController();
 
             // Act
             controller.Delete(5);
