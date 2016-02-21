fdf260a0567413c780a9a69894157b71d0586ff9
Yaaappee <yaaappee@gmail.com>
2016-02-21 10:21:09 +0200

    Modify main page / minor fixes [2]

---
+++ Data.gov.ua/Controllers/BillsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Data.gov.ua.Models;
+
+namespace Data.gov.ua.Controllers
+{
+    public class BillsController : ApiController
+    {
+        // GET api/bills
+        public IEnumerable<Bill> Get()
+        {
+            var parser = new PersonsXMLParser();
+            var listOfBills = parser.GetBills();
+            return listOfBills;
+        }
+
+        // GET api/bills
+        public IEnumerable<Bill> Get(int? limit)
+        {
+            var parser = new PersonsXMLParser();
+            int value;
+            if (limit != null && int.TryParse(limit.ToString(), out value))
+            {
+                var listOfBills = parser.GetBills().GetRange(0, value);
+                return listOfBills;
+            }
+            return null;
+        }
+
+        // GET api/bills/5
+        public Bill Get(int id)
+        {
+            var parser = new PersonsXMLParser();
+            return parser.GetBills()[id];
+        }
+    }
+}
